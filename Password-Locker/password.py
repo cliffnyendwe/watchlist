8374653b024b7743b7b41e50e784a3a182ab1e3d
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2018-11-01 15:21:59 +0300

    docstring removed for simplicity

---
+++ Password-Locker/password.py
@@ -0,0 +1,13 @@
+class Password:
+    """
+    clas that generates new instances of password
+    """
+
+    Password_list = []
+
+    def _init_(self,first_name,last_name,password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+
+
