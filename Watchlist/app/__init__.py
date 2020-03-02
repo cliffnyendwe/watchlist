d5dccc55f69ef3efa188e7be4ad90c9516347108
cliffnyendwe <cliffnyendwe2018@gmail.com>
2020-03-02 10:34:52 +0300

    deleted the migration

--- Watchlist/app/__init__.py
+++ Watchlist/app/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_uploads import UploadSet,configure_uploads,IMAGES
+# from flask_uploads import configure_uploads,IMAGES
 from flask_mail import Mail
 
 db = SQLAlchemy()
@@ -11,7 +11,7 @@ bootstrap = Bootstrap()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-photos= UploadSet('photos',IMAGES)
+# photos= UploadSet('photos',IMAGES)
 mail = Mail()
 
 def create_app(config_name):
@@ -28,7 +28,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
-    
+
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -40,6 +40,6 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
     # config uploadset
-    configure_uploads(app,photos)
+    # configure_uploads(app,photos)
 
     return app
--- Watchlist/config.py
+++ Watchlist/config.py
@@ -13,7 +13,7 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    
+
     @staticmethod
     def init_app(app):
         pass
